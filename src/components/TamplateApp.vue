cd5a195d859f6a1b02629e1dd94dcf96866205fa
muhammadBilal924 <mbilal60924@gmail.com>
2022-02-17 16:57:25 +0500

    first commit

---
+++ src/components/TamplateApp.vue
@@ -0,0 +1,33 @@
+
+<template>
+    <div>
+   <h1>Tamplate in vue js</h1>
+    <h3>{{name}}</h3>
+    <h3>{{email}}</h3>
+    <button v-on:click="updateText">Click me</button>
+    </div>
+</templateApp>
+
+<script>
+    export default {
+        name:'TamplateApp',
+        data()
+        {
+            return{
+                name:'M.Bilal',
+                
+            }
+        },
+        methods:{
+            updateText()
+           {
+            this.name="M.Sufyan"
+            }
+        }
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
